3d227cb16738e47ddc9ff11364fa6e302a365c29
Aptroide <pato.carranco@gmail.com>
2024-12-05 20:40:27 -0500

    change model path

--- app/routes/prediction.py
+++ app/routes/prediction.py
@@ -5,8 +5,6 @@ import pickle
 import pandas as pd
 from datetime import datetime
 from .. import schemas
-import os
-
 
 router = APIRouter(
     prefix="/predict",
@@ -14,14 +12,13 @@ router = APIRouter(
     responses={404: {"description": "No encontrado"}},
 )
 
-# Load the model at startup
+# Cargar el modelo al iniciar
 try:
-    model_path = os.path.join(os.path.dirname(__file__), 'prophet_model.pkl')
-    with open(model_path, 'rb') as f:
+    with open('/usr/src/app/prophet_model.pkl', 'rb') as f:
         model = pickle.load(f)
-except Exception as e:
-    print(f'Error loading model: {e}')
-    model = None
+except FileNotFoundError:
+    with open('../prophet_model.pkl', 'rb') as f:
+        model = pickle.load(f)
 
 @router.post("/", response_model=schemas.PredictionResponse)
 def make_prediction(pred_request: schemas.PredictionRequest):

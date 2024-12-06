43ac4408d9da40e844ea40101ab0be76cd41c07b
Aptroide <pato.carranco@gmail.com>
2024-12-05 20:33:39 -0500

    Heroku try

--- app/main.py
+++ app/main.py
@@ -31,4 +31,4 @@ app.include_router(prediction.router)
 
 @app.get("/")
 def root():
-    return {"message": "Hello API"}
+    return {"message": "Hello World deployed  from CI/CD with Github Actions"} 

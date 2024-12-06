0a91e7154ff27e8191eb1a33b79a4b72de0053cf
Aptroide <pato.carranco@gmail.com>
2024-12-05 22:10:11 -0500

    cambio de path model 8

--- app/routes/prediction.py
+++ app/routes/prediction.py
@@ -5,6 +5,12 @@ import pickle
 import pandas as pd
 from datetime import datetime
 from .. import schemas
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+model_path = os.path.join(BASE_DIR, "prophet_model.pkl")
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
 
 router = APIRouter(
     prefix="/predict",
@@ -12,9 +18,9 @@ router = APIRouter(
     responses={404: {"description": "No encontrado"}},
 )
 
-# Cargar el modelo al iniciar
-with open('/home/runner/work/control_ventas/control_ventas/prophet_model.pkl', 'rb') as f:
-    model = pickle.load(f)
+# # Cargar el modelo al iniciar
+# with open('/home/runner/work/control_ventas/control_ventas/prophet_model.pkl', 'rb') as f:
+#     model = pickle.load(f)
 
 @router.post("/", response_model=schemas.PredictionResponse)
 def make_prediction(pred_request: schemas.PredictionRequest):
